63bb9a7b10e4fdc0e0553e0abb469adb6b98ec3d
jyoon186 <74604994+jyoon186@users.noreply.github.com>
2020-12-09 17:41:07 +0900

    Create code.php

---
+++ code.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title> test </title>
+</head>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5fd080f1a1d54c18d8f1d85b/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
+<body>
+<h1> test용 </t1>
+</body>
+
+</html>
